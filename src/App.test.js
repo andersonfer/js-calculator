9ae4f746cd2e1bb878841ca8390c45ab6aa9f774
Anderson Fér <andersonfer@gmail.com>
2023-04-10 12:20:45 +0100

    Create an arrayto hold all the number buttons

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('should render properly', () => {
+const NUMBER_BUTTONS = [];
+
+beforeAll(() =>{
   render(<App />);
 
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '0'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '1'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '2'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '3'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '4'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '5'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '6'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '7'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '8'}));
+  NUMBER_BUTTONS.push(screen.getByRole('button',{name: '9'}));
+});
+
+it('should render properly', () => {
   expect(screen.getByTestId('display')).toHaveTextContent('0');
 
   screen.getByRole('button',{name: 'AC'});
@@ -15,14 +30,4 @@ it('should render properly', () => {
   screen.getByRole('button',{name: '='});
   screen.getByRole('button',{name: '.'});
 
-  screen.getByRole('button',{name: '0'});
-  screen.getByRole('button',{name: '1'});
-  screen.getByRole('button',{name: '2'});
-  screen.getByRole('button',{name: '3'});
-  screen.getByRole('button',{name: '4'});
-  screen.getByRole('button',{name: '5'});
-  screen.getByRole('button',{name: '6'});
-  screen.getByRole('button',{name: '7'});
-  screen.getByRole('button',{name: '8'});
-  screen.getByRole('button',{name: '9'});
 });
