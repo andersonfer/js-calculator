548b91861a13859b337d880fd4e38d150dcf47fd
Anderson Fér <andersonfer@gmail.com>
2023-04-11 19:33:56 +0100

    Change the App from class to function

--- src/App.js
+++ src/App.js
@@ -1,128 +1,115 @@
-import React from 'react'
+import {useState} from 'react'
 import './App.css';
 import * as mathjs from "mathjs";
 
 export const MAX_DISPLAY_LENGTH = 15;
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: '0',
-      resetExpression:false
-    };
-  }
 
-  render() {
-    return (
-      <div id="calculator">
-        <Display input={this.state.input}  />
-        <NumPad onClick={this.updateDisplay} onClear={this.clearDisplay}/>
-      </div>
-    );
-  }
+function App() {
+  const [input, setInput] = useState('0');
+  const [resetExpression, setResetExpression] = useState(false);
 
-  updateDisplay = (key) => {
-    if (this.hasReachedMaximumLength(this.state.input)){
-      this.setState({
-        input:'MAX',
-        resetExpression:true
-      }
-      );
+  const updateDisplay = (key) => {
+    if (hasReachedMaximumLength(input)) {
+      setInput('MAX');
+      setResetExpression(true);
     } else {
-      this.isEqualSign(key)?
-      this.evaluateExpression():
-      this.isNegativeSign(key)?
-      this.updateNegativeSign(key):
-      this.isOperator(key)?
-      this.updateOperator(key):
-      this.isDecimal(key)?
-      this.updateDecimal(key):
-      this.updateNumber(key);
+      isEqualSign(key)
+        ? evaluateExpression()
+        : isNegativeSign(key)
+        ? updateNegativeSign(key)
+        : isOperator(key)
+        ? updateOperator(key)
+        : isDecimal(key)
+        ? updateDecimal(key)
+        : updateNumber(key);
     }
   };
 
-  hasReachedMaximumLength = (input) => {
+  const hasReachedMaximumLength = (input) => {
     return input.length >= MAX_DISPLAY_LENGTH;
-  }
+  };
 
-  isEqualSign = (key) => {return '=' === key};
+  const isEqualSign = (key) => {
+    return '=' === key;
+  };
 
-  evaluateExpression = () => {
+  const evaluateExpression = () => {
     const parser = mathjs.parser();
-    this.setState((state) => ({
-      input:
-        //if the last character is a number, evaluate the expression.
-        //else reset the expression to zero.
-        this.isNumber(state.input[state.input.length-1]) ?
-        parser.evaluate(state.input).toString().substr(0,MAX_DISPLAY_LENGTH):
-        '0',
-      resetExpression:true
-    }));
-  }
-
-  isNegativeSign = (key) => {return '-' === key};
-
-  updateNegativeSign(key){
-    this.setState((state) => ({
-      input:
-        state.input === 0?
-        key:
-        //if the last element is a number
-        this.isNumber(state.input[state.input.length-1]) ?
-        state.input.concat(key):
-        //if the 2 last elements are an operator and a number
-        this.isOperator(state.input[state.input.length-1]) && this.isNumber(state.input[state.input.length-2])?
-        state.input.concat(key):
-        state.input,
-      resetExpression: false
-    }));
-  }
-
-  isOperator = (key) => {return ['+','*','-','/'].includes(key)};
-
-  updateOperator(key){
-    this.setState((state) => ({
-      input:
-        state.input != 0 && this.isNumber(state.input[state.input.length-1]) ?
-        state.input.concat(key):
-        //in case of three consecutive operators, the operation that should be performed is the last one
-        this.isOperator(state.input[state.input.length-1]) && this.isOperator(state.input[state.input.length-2])?
-        state.input.substr(0,state.input.length-2).concat(key):
-        state.input.substr(0,state.input.length-1).concat(key),
-      resetExpression: false
-    }));
-  }
-
-  isDecimal = (key) => {return key === '.'};
-
-  updateDecimal(key){
-    //get the lastNumber in the expression and check if it already has decimal
-    //TODO this expression ignores the negative sign, should fix this
-    const lastNumber = this.state.input.split(/\+|-|\*|\//).pop();
-    if(lastNumber && lastNumber.indexOf(key) === -1){
-      this.setState((state) => ({
-      input: state.input.concat(key),
-      resetExpression: false
-    }));
-    }
-  }
-
-  isNumber = (key) => {return ['0','1','2','3','4','5','6','7','8','9'].includes(key)};
-
-  updateNumber(key){
-    this.setState((state) => ({
-      input:
-        state.input === '0' || state.resetExpression ? key:
-        state.input.concat(key),
-      resetExpression:false
-    }));
-  }
-
-  clearDisplay = () => {
-    this.setState({
-      input:'0'
+    setInput((state) => {
+      return isNumber(state[state.length - 1])
+        ? parser.evaluate(state).toString().substr(0, MAX_DISPLAY_LENGTH)
+        : '0';
     });
-  }
+    setResetExpression(true);
+  };
+
+  const isNegativeSign = (key) => {
+    return '-' === key;
+  };
+
+  const updateNegativeSign = (key) => {
+    setInput((state) => {
+      return state === 0
+        ? key
+        : isNumber(state[state.length - 1])
+        ? state.concat(key)
+        : isOperator(state[state.length - 1]) && isNumber(state[state.length - 2])
+        ? state.concat(key)
+        : state;
+    });
+    setResetExpression(false);
+  };
+
+  const isOperator = (key) => {
+    return ['+', '*', '-', '/'].includes(key);
+  };
+
+  const updateOperator = (key) => {
+    setInput((state) => {
+      return state != 0 && isNumber(state[state.length - 1])
+        ? state.concat(key)
+        : isOperator(state[state.length - 1]) && isOperator(state[state.length - 2])
+        ? state.substr(0, state.length - 2).concat(key)
+        : state.substr(0, state.length - 1).concat(key);
+    });
+    setResetExpression(false);
+  };
+
+  const isDecimal = (key) => {
+    return key === '.';
+  };
+
+  const updateDecimal = (key) => {
+    const lastNumber = input.split(/\+|-|\*|\//).pop();
+    if (lastNumber && lastNumber.indexOf(key) === -1) {
+      setInput((state) => {
+        return state.concat(key);
+      });
+      setResetExpression(false);
+    }
+  };
+
+  const isNumber = (key) => {
+    return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(key);
+  };
+
+  const updateNumber = (key) => {
+    setInput((state) => {
+      return state === '0' || resetExpression ? key : state.concat(key);
+    });
+    setResetExpression(false);
+  };
+
+  const clearDisplay = () => {
+    setInput('0');
+  };
+
+  return (
+    <div id="calculator">
+      <Display input={input} />
+      <NumPad onClick={updateDisplay} onClear={clearDisplay} />
+    </div>
+  );
 }
 
 function Display({input}){
